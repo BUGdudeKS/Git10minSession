7672c96fb4540cf54b9af249294432b06c5d9adc
gentrit.osmani <TheworldisBig99>
2022-04-20 16:23:27 +0200

    second commit via IntelliJ

--- src/firstCommit.java
+++ src/firstCommit.java
@@ -1,2 +1,5 @@
 public class firstCommit {
+    public static void main(String[] args) {
+        System.out.println("We love GIT");
+    }
 }
---
+++ src/secondCommit.java
@@ -0,0 +1,5 @@
+public class secondCommit {
+    public static void main(String[] args) {
+        System.out.println("This will be the second commit");
+    }
+}
